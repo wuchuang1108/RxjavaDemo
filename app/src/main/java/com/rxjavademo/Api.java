c5e6347efc83b87f54b1e14cf140882f436f8e6b
wuchuang <wu_chuang1108@gmail.com>
2017-12-22 10:08:32 +0800

    test

--- app/src/main/java/com/rxjavademo/Api.java
+++ app/src/main/java/com/rxjavademo/Api.java
@@ -1,6 +1,10 @@
 package com.rxjavademo;
 
 
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 /**
  * Created by wuchaung on 2017/11/26.
  * <p>
@@ -8,5 +12,6 @@ package com.rxjavademo;
  */
 
 public interface Api {
-   
+    @GET("login/")
+    Call getData(@Query("name") String name, @Query("password") String pw);
 }
